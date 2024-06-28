d4447b5df53c3df36d3a0778f31ff2e2f1180457
feelomi <137166175+feelomi@users.noreply.github.com>
2024-06-29 01:29:29 +0300

    Creating Spring project

---
+++ web-server/src/test/java/com/example/catsspring/CatsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
